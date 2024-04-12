bd658ce65440123245c0bc1aea58d896cb769903
Francy Santos <francy.horray@gmail.com>
2024-04-12 20:59:10 -0300

    pull commit

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,24 +1,20 @@
-'use client';
+"use client";
 
-import React, { Suspense } from 'react';
-import { Sidebar } from './_components/sidebar';
-
-import { usePathname } from 'next/navigation';
-import Materiais from './materiais/page';
-import EvolucaoDeObras from './evolucao-de-obras/page';
-import MainContent from './main-content/page';
-import Blog from './blog/page';
+import { usePathname } from "next/navigation";
+import Materiais from "./materiais/page";
+import EvolucaoDeObras from "./evolucao-de-obras/page";
+import MainContent from "./main-content/page";
+import Blog from "./blog/page";
 
 const DashboardPage = () => {
   const path = usePathname();
-  console.log(path);
 
   return (
     <main className="flex w-full ">
-      {path === '/dashboard' && <MainContent />}
-      {path === '/dashboard/materiais' && <Materiais />}
-      {path === '/dashboard/evolucao-de-obras' && <EvolucaoDeObras />}
-      {path === '/dashboard/blog' && <Blog />}
+      {path === "/dashboard" && <MainContent />}
+      {path === "/dashboard/materiais" && <Materiais />}
+      {path === "/dashboard/evolucao-de-obras" && <EvolucaoDeObras />}
+      {path === "/dashboard/blog" && <Blog />}
     </main>
   );
 };
