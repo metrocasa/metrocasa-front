750939e09ddae5d5ffdddb8ef543153a5ac7a315
Francy Santos <francy.horray@gmail.com>
2024-05-17 15:09:12 -0300

    fix: removed base_url from hero poster

--- components/carousel.tsx
+++ components/carousel.tsx
@@ -38,7 +38,7 @@ const CarouselHero = ({ imovel }: { imovel: Imovel }) => {
             {!isMobile ? (
               <BackgroundVideo
                 src={isMobile ? '' : imoveisData?.attributes.video_hero}
-                poster={`${BASE_URL}${imoveisData?.attributes.fachada.data?.attributes.url}`}
+                poster={`${imoveisData?.attributes.fachada.data?.attributes.url}`}
                 className="active w-full h-full rounded-xl object-cover bg-center "
               >
                 <div className="absolute top-0 left-0 w-full h-full bg-gradient-bg-reversed rounded-lg" />
