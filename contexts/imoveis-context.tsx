e486e8a9d55d13a61b4bd1f12c9324ff0ded74ce
Francy Santos <francy.horray@gmail.com>
2024-05-28 15:29:27 -0300

    fix: imoveis context w token

--- contexts/imoveis-context.tsx
+++ contexts/imoveis-context.tsx
@@ -41,8 +41,16 @@ export const ImoveisProvider: React.FC<{ children: React.ReactNode }> = ({
   // Função para buscar imóveis
   const fetchImoveis = async (pageSize = currentPageSize) => {
     try {
+      // CONFIG DA API TOKEN DE IMOVEIS
+      const headers = new Headers({
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN_IMOVEIS}`,
+      });
+
       const response = await fetch(
         `${BASE_URL}/api/imoveis?pagination[page]=1&pagination[pageSize]=${pageSize}&populate[planta_comp][populate][planta_image][fields][0]=url&populate[fachada][populate][fields][0]=url&populate[logo][populate][fields][0]=url&populate[main_gallery][populate][fields][0]=url&populate[materiais][populate]=*`,
+        {
+          headers: headers,
+        },
       );
       if (!response.ok) {
         throw new Error('Erro ao buscar imóveis');
@@ -70,7 +78,7 @@ export const ImoveisProvider: React.FC<{ children: React.ReactNode }> = ({
           headers: headers,
           cache: 'force-cache',
           next: {
-            revalidate: 3600, // 1h
+            revalidate: 600, // 1h
           },
         },
       );
