81e35635f81fa346750917ba9e11bbd8053cc562
Francy Santos <francy.horray@gmail.com>
2024-06-13 21:35:46 -0300

    :bug: fix number meta context

--- contexts/meta-context.tsx
+++ contexts/meta-context.tsx
@@ -25,7 +25,7 @@ export const MetaProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   const [meta, setMeta] = useState<number>();
-  const [currentPageSize, setCurrentPageSize] = useState<number>(8);
+  const [currentPageSize, setCurrentPageSize] = useState<number>(12);
 
   return (
     <MetaContext.Provider
