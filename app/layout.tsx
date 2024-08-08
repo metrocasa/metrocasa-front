1649ea6c85cdf7d008ccff08410e0a9b70f99a10
Francy Santos <francy.horray@gmail.com>
2024-08-08 15:36:55 -0300

    :tada: GTM v3

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
 
+import { GoogleTagManager } from "@next/third-parties/google";
+
 import { MetaProvider } from "@/contexts/meta-context";
 import { CSPostHogProvider } from "./PosthogProvider";
 import { Suspense } from "react";
@@ -31,36 +33,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br" className="scroll-smooth">
+      <GoogleTagManager gtmId={GTM_ID} />
+
       <CSPostHogProvider>
         <body className={`${inter.className} px-[15px]`}>
-          {/* Google Tag Manager Script */}
-          <Script
-            id="gtm-script"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `
-                (function(w,d,s,l,i){
-                  w[l]=w[l]||[];
-                  w[l].push({'gtm.start':
-                  new Date().getTime(),event:'gtm.js'});
-                  var f=d.getElementsByTagName(s)[0],
-                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','${GTM_ID}');
-              `,
-            }}
-          />
-
-          {/* Google Tag Manager (noscript) */}
-          <noscript
-            dangerouslySetInnerHTML={{
-              __html: `
-                <iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}"
-                height="0" width="0" style="display:none;visibility:hidden"></iframe>
-              `,
-            }}
-          />
-
           <MetaProvider>
             <Suspense>
               <div>
